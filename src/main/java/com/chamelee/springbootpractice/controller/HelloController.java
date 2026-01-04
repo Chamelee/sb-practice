18b454fabb57768b5547e0afd0d5489dbba6c888
Chamelee <chxxxlxx@gmail.com>
2026-01-04 20:33:31 +0530

    feat: add endpoints using query and path parameters

--- src/main/java/com/chamelee/springbootpractice/controller/HelloController.java
+++ src/main/java/com/chamelee/springbootpractice/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.chamelee.springbootpractice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,18 @@ public class HelloController {
     public String getHelloFromVar(){
         String text = "Hello from Spring Boot as a variable";
         return text;
+    }
+
+    @GetMapping("/addNumQue")
+    public int addNumQue(@RequestParam int num){
+        int result = 5 + num;
+        return result;
+    }
+
+    @GetMapping("/addNumPath/{num}")
+    public int addNumPath(@PathVariable int num){
+        int result = 5 + num;
+        return result;
     }
 
 }
